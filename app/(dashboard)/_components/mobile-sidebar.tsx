2f21398787afba84ec49070f865b53e95db91f95
Yadav106 <sahil.jp.yadav@gmail.com>
2024-07-02 08:38:12 +0530

    added some more details about doctor, and made the sheet close on click

--- app/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,7 +1,9 @@
 import { Menu } from "lucide-react";
 
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "./sidebar";
+import Logo from "./logo";
+import SidebarRoutes from "./sidebar-routes";
 
 const MobileSidebar = () => {
     return ( 
@@ -13,7 +15,17 @@ const MobileSidebar = () => {
                 side={'left'}
                 className="p-0"
             >
-                <Sidebar />
+                {/* <Sidebar /> */}
+                <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
+                    <div className="p-6">
+                        <Logo />
+                    </div>
+                    <div className="flex flex-col w-full">
+                        <SheetClose>
+                            <SidebarRoutes />
+                        </SheetClose>
+                    </div>
+                </div>
             </SheetContent>
         </Sheet>
      );
--- app/(dashboard)/_components/prof-details.tsx
+++ app/(dashboard)/_components/prof-details.tsx
@@ -58,7 +58,7 @@ const ProfessionalDetails = () => {
                         <p className="mt-2 flex font-bold text-3xl">{experience}</p>
                     </div>
                 </div>
-                <div className='bg-gray-300/80 p-3 px-5 rounded-xl'>
+                {/* <div className='bg-gray-300/80 p-3 px-5 rounded-xl'>
                     <p className='underline text-sm'>Phone</p>
                     <div className='flex items-baseline gap-2'>
                         <Phone width={40}/>
@@ -70,6 +70,43 @@ const ProfessionalDetails = () => {
                     <div className='flex items-baseline gap-2'>
                         <Mail width={40}/>
                         <p className="mt-2 flex font-bold text-3xl">{mail}</p>
+                    </div>
+                </div> */}
+
+                <div className='flex flex-col gap-2 mt-6'>
+                    <span className='font-semibold text-[30px] text-gray-600 underline'>
+                        About the doctor
+                    </span>
+                    <div className='flex flex-col gap-5 p-4 bg-gray-300/80 rounded-xl'>
+                        <div className='flex flex-col gap-1'>
+                            <span className='font-semibold text-gray-700'>Name : </span>
+                            <span className='font-semibold ml-5'>{`Salsabila Bajaber (aka Bella Bee)`}</span>
+                        </div>
+
+                        <div className='flex flex-col gap-1'>
+                            <span className='font-semibold text-gray-700'>Age : </span>
+                            <span className='font-semibold ml-5'>{`24 years, or the equivalent of a Russian grandmother`}</span>
+                        </div>
+
+                        <div className='flex flex-col gap-1'>
+                            <span className='font-semibold text-gray-700'>Passion : </span>
+                            <span className='font-semibold ml-5'>{`Reading, Writing, Threatening`}</span>
+                        </div>
+
+                        <div className='flex flex-col gap-1'>
+                            <span className='font-semibold text-gray-700'>Extra-curricular activities : </span>
+                            <span className='font-semibold ml-5'>{`Tax Evasion & Organ Smuggling`}</span>
+                        </div>
+
+                        <div className='flex flex-col gap-1'>
+                            <span className='font-semibold text-gray-700'>Favourite Pastime : </span>
+                            <span className='font-semibold ml-5'>{`Hobnobbing with Mafia members`}</span>
+                        </div>
+
+                        <div className='flex flex-col gap-1'>
+                            <span className='font-semibold text-gray-700'>Accepted payments : </span>
+                            <span className='font-semibold ml-5'>{`Dollars, Bitcoin & Hideo Kojima Memorabilia`}</span>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -91,7 +128,7 @@ const ProfessionalDetails = () => {
                         </div>
                     </div>
                 </div>
-                <div className='flex flex-col gap-1'>
+                {/* <div className='flex flex-col gap-1'>
                     <div className='bg-gray-300/80 p-3 px-5 rounded-xl'>
                         <p className='underline text-sm'>Phone</p>
                         <div className='flex items-baseline gap-2'>
@@ -104,6 +141,42 @@ const ProfessionalDetails = () => {
                         <div className='flex items-baseline gap-2'>
                             <Mail width={40}/>
                             <p className="mt-2 flex font-bold text-3xl">{mail}</p>
+                        </div>
+                    </div>
+                </div> */}
+                <div className='flex flex-col gap-2 mt-6'>
+                    <span className='font-semibold text-[30px] text-gray-600 underline'>
+                        About the doctor
+                    </span>
+                    <div className='flex flex-col gap-5 p-4 bg-gray-300/80 rounded-xl'>
+                        <div className='flex flex-col gap-1'>
+                            <span className='font-semibold text-gray-700'>Name : </span>
+                            <span className='font-semibold ml-5'>{`Salsabila Bajaber (aka Bella Bee)`}</span>
+                        </div>
+
+                        <div className='flex flex-col gap-1'>
+                            <span className='font-semibold text-gray-700'>Age : </span>
+                            <span className='font-semibold ml-5'>{`24 years, or the equivalent of a Russian grandmother`}</span>
+                        </div>
+
+                        <div className='flex flex-col gap-1'>
+                            <span className='font-semibold text-gray-700'>Passion : </span>
+                            <span className='font-semibold ml-5'>{`Reading, Writing, Threatening`}</span>
+                        </div>
+
+                        <div className='flex flex-col gap-1'>
+                            <span className='font-semibold text-gray-700'>Extra-curricular activities : </span>
+                            <span className='font-semibold ml-5'>{`Tax Evasion & Organ Smuggling`}</span>
+                        </div>
+
+                        <div className='flex flex-col gap-1'>
+                            <span className='font-semibold text-gray-700'>Favourite Pastime : </span>
+                            <span className='font-semibold ml-5'>{`Hobnobbing with Mafia members`}</span>
+                        </div>
+
+                        <div className='flex flex-col gap-1'>
+                            <span className='font-semibold text-gray-700'>Accepted payments : </span>
+                            <span className='font-semibold ml-5'>{`Dollars, Bitcoin & Hideo Kojima Memorabilia`}</span>
                         </div>
                     </div>
                 </div>
